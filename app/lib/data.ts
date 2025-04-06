75ce1be5716a9e9a8372f60ac107f1c68c2d1d88
Haotian <huntchen00@gmail.com>
2025-04-06 03:56:55 -0400

    feat: Add dashboard overview and invoice management features
    - Implement loading state with DashboardSkeleton for the dashboard overview.
    - Create main dashboard page with cards for customer and invoice statistics.
    - Add NotFound component for handling missing invoices.
    - Develop edit invoice page with form and breadcrumbs for navigation.
    - Create page for creating new invoices with customer selection.
    - Implement error handling component for invoice operations.
    - Add server-side actions for creating, updating, and deleting invoices with validation.
    - Configure ESLint for code quality and consistency.

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,12 +16,12 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
@@ -32,12 +32,21 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 6000));
+
     const data = await sql<LatestInvoiceRaw[]>`
-      SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
-      FROM invoices
-      JOIN customers ON invoices.customer_id = customers.id
-      ORDER BY invoices.date DESC
-      LIMIT 5`;
+    SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    ORDER BY invoices.date DESC
+    LIMIT 5`;
+
+    console.log('Data fetch completed after 6 seconds.');
+
+
+
 
     const latestInvoices = data.map((invoice) => ({
       ...invoice,
@@ -72,6 +81,7 @@ export async function fetchCardData() {
     const numberOfCustomers = Number(data[1][0].count ?? '0');
     const totalPaidInvoices = formatCurrency(data[2][0].paid ?? '0');
     const totalPendingInvoices = formatCurrency(data[2][0].pending ?? '0');
+
 
     return {
       numberOfCustomers,
@@ -216,3 +226,5 @@ export async function fetchFilteredCustomers(query: string) {
     throw new Error('Failed to fetch customer table.');
   }
 }
+
+
